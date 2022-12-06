866961ec39d054c0af67f24e323b570de547e43f
hyeonRS <hhsk0908@naver.com>
2022-12-06 23:57:30 +0900

    BGM / cha / ending

---
+++ BGMPlayer/SoundManager.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+#singleton
+
+onready var music_list = $Music
+onready var title_music_list = $Music/TitleScreenBGM
+onready var GamePlay_music_list = $Music/GamePlayMusic
+onready var Ending_music_list = $Music/EndingMusic
+
+onready var Jump_sound = $SoundEffect/jump
+
+func update_sound_volume(value, vol_range, type):
+	match type:
+		"Music":
+			pass
+		"SoundEffect":
+			pass
---
+++ ending/ending screen.gd
@@ -0,0 +1,36 @@
+extends Control
+
+
+var counter = 0
+var scroll_speed : int = 10
+onready var run_animation = $CenterContainer/AnimatedSprite
+
+var credit_list = [
+	["Made by","Seogeonhyuk"],
+	["Made by","Hyeonhongseok"],
+	["Made by","kimkyeongyeon"],
+	["Background Music","Imagine dragons - Borns(remix)"],
+	["Ending Music","Song_title"],
+	["Font","esamanru Bold"],
+	["Thank you","CMP"],
+	["Thank you","Teemu"],
+]
+
+func _ready() -> void:
+	$Timer.start()
+	run_animation.flip_h = true
+	SoundManager.Ending_music_list.play()
+
+	
+func _process(delta:float) -> void:
+	$ParallaxBackground/ParallaxLayer.motion_offset.x -= scroll_speed *delta
+	run_animation.play("RunRight")
+
+func _on_Timer_timeout():
+	$VBoxContainer/Title.text = credit_list[counter][0]
+	$VBoxContainer/Name.text = credit_list[counter][1]
+	$AnimationPlayer.play("FadeInOut")
+	counter += 1
+	if counter == credit_list.size() :
+		$Timer.stop()
+		
