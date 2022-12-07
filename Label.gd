856d40c2d0e398e7c6ccf2a3801dc0569ecc6774
hyeonRS <hhsk0908@naver.com>
2022-12-08 01:53:05 +0900

    ending 음악 추가

---
+++ Label.gd
@@ -0,0 +1,25 @@
+extends Label
+
+
+var time = 120
+var timer_on = true
+
+
+func _process(delta):
+	if(timer_on):
+		time -= delta
+	var mils = fmod(time, 1)*100
+	var secs = fmod(time,60)
+	var mins = fmod(time,60*60) / 60
+
+		
+	
+	
+	if time <= 0.01:
+		timer_on = false
+		mils = 0
+		
+	var time_passed = "%0.2d : %0.2d : %0.3d" %[mins,secs,mils]
+	text = time_passed
+	
+	pass
