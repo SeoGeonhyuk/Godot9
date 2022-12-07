189a11a11d79270fb1815666027cda82bfdb7c8f
kky107102 <kky107102@ajou.ac.kr>
2022-12-08 02:26:03 +0900

    win/lose 스크린 제작/ 월드 복구되면 크라운과 연결

---
+++ ResultScreen/result.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+#if jungle_ended:
+#	$worlds/Jungle.visible = true
+#	$worlds/Fire.visible = false
+#	$worlds/Ice.visible = false
+#elif lava_ended:
+#	$worlds/Jungle.visible = false
+#	$worlds/Fire.visible = true
+#	$worlds/Ice.visible = false
+#elif ice_ended:
+#	$worlds/Jungle.visible = false
+#	$worlds/Fire.visible = false
+#	$worlds/Ice.visible = true
+#
+#if lose:
+#	$win/win.visible = false
+#	$win/Crown.visible = false
+#	$lose/lose.visible = true
+#
